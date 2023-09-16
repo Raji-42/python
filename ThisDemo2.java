980a33099c75db881ccf7509452280183486a14d
Raji-42 <143578094+Raji-42@users.noreply.github.com>
2023-09-16 15:25:03 +0530

    Create ThisDemo2.java

---
+++ ThisDemo2.java
@@ -0,0 +1,30 @@
+//ThisDemo1
+//16-9-23
+//rajeswari
+import java.io.*;
+class Student{
+    int RollNumber;
+    String Name;
+    float Fee;
+    Student(int RollNumber,String Name,float Fee)
+    {
+       this.RollNumber = RollNumber;
+       this.Name = Name;
+        this.Fee = Fee;
+    }
+    void display()
+    {
+        System.out.println("Student details are");
+        System.out.println(RollNumber+" "+Name+" "+Fee);
+    }
+}
+    public class ThisDemo2
+    {
+        public static void main(String args[])
+        {
+            Student s1 = new Student(4242,"N.Rajeswari",45700);
+            Student s2 = new Student(4234,"K.Sushma",45700);
+            s1.display();
+            s2.display();
+        }
+    }
